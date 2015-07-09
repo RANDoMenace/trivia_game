5fa5201037fcafbe0fe51c026fe5be78cbde527b
Bryan Garza <bgarza1016@gmail.com>
2015-07-09 15:41:30 -0700

    added attention to detail, and user test suggestions

--- README.md
+++ README.md
@@ -1,11 +1,52 @@
-User stories
+# Trivia Game
 
-Trivia Game
+##User stories
 
-As a user I would like a siimple game, i will use common sense as the subject.
-(example)
-righty tiety, lefty ______? a)mighty b)bitey c)loosey d)truthy
+* As a user I would like a siimple game, i will use common sense as the subject.
 
-As a user I would like a visable scoring system, i will create a visable score board.
+	* (example)
+righty tiety, lefty ______? a)mighty b)bitey c)loosey
 
-As a user I want to know who is the winner, i will make an alert once the game is won.
+* As a user I would like a visable scoring system, i will create a visable score board.
+
+* As a user I want to know who is the winner, i will make an alert once the game is won.
+
+##Progress
+
+* HTML and CSS are all centered for easy viewing while coding.
+
+* Working on logic of game. Decided to go with array for questions and answer key.
+
+* I chose simple questions to go with a simple answer key.
+
+* I then chose to make a continue button to move the game along, once a answer is selected.
+
+* I went with replacing the text in the 'h2' tag.
+
+* I decided to start the page off with the first question, and all variables start at 0.
+
+* Then I connected a counter to the continue button which cycles through the questions array.
+
+* Moved onto making the buttons work in the console. Then created an empty array to store the answers.
+
+* Used the text inside the button to store as the answer inside the empty array.
+
+* Got the answer sheet to score right answers, and pass on wrong answers.
+
+* Used pop.() to capture last button pressed in case of player changing their mind about a question.
+
+* Counter runs the score comparison, the info inside the arrays being compared, and the questions being displayed.
+
+* Used an if/else statement to check questions end. Used a confirm alert to check if player wants to play again.
+
+* If confirm is true then everything is reset to -1 to allow the first click of continue to register all variables to 0.
+
+* Asked a UX student to help with directing user to click continue to move forward.
+
+#Current
+
+* Working on winner being displayed inside confirm box.
+* Styling and user design.
+* Maybe use fade in and outs for question display.
+* Could randomize the questions.
+* Could disable continue button until both player have choosen an answer. 
--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
-
 console.log('activate framework!');
 
 /*setting up questions and answers for loops*/
 
-var triviaQ = new Array(10);
+var triviaQ = new Array(11);
 var answerKey = new Array(10);
 
 var answerSheet1 = [];
 var answerSheet2 = [];
 
-/*grabbing answers from players*/
+/*starting score for players*/
+
 var player1Score = 0;
 var player2Score = 0;
-var winner;
-var gameOn = false;// true in event
 
-triviaQ[0] = "Finish this sentence: Righty tighty, lefty _______.<br>A) Loosey B) Choosey C) Moosey";
+/*Q & A Array*/
+
+triviaQ[0] = "Righty is to tighty, as lefty is to ______.<br>A) Loosey B) Choosey C) Moosey";
 triviaQ[1] = "If your computer doesn't turn on, what do you check first?<br>A) Restart B) Power C) Wifi";
 triviaQ[2] = "The tool that lifts your car is called?<br>A) Kicker B) Chalk C) Jack";
-triviaQ[3] = "The grass is ______?<br>A) Green B) Brown C) Yellow";
-triviaQ[4] = "The sky is ____?<br>A) Grey B) Blue C) Pink";
+triviaQ[3] = "The grass is the color_____?<br>A) Green B) Brown C) Yellow";
+triviaQ[4] = "The sky is the color____?<br>A) Grey B) Blue C) Pink";
 triviaQ[5] = "The largest organ the human body possesses is?<br>A) Lungs B) Tounge C) Skin";
 triviaQ[6] = "What is the fastest organ in the human body?<br>A) Eyes B) Heart C) Bladder";
-triviaQ[7] = "Do they have a 4th of July in Great Britain?<br>A) Yes B) No C) Thier own kind";
-triviaQ[8] = "How many stars are on the American flag?<br>A) 48 B) 50 C) 51";
+triviaQ[7] = "Do they have a 4th of July in Great Britain?<br>A) No  B) Yes C) Maybe";
+triviaQ[8] = "How many stars are on the American flag?<br>A) 48 B) 49 C) 50";
 triviaQ[9] = "Which is the biggest island of Hawaii?<br>A) Big Island B) Kawaii C) Maui";
+triviaQ[10] = "Good game!";
 
 answerKey[0] = "A";
 answerKey[1] = "B";
@@ -37,13 +38,7 @@ answerKey[7] = "B";
 answerKey[8] = "C";
 answerKey[9] = "A";
 
-
 /*----DOM connections-----*/
-var scoreB1 = document.getElementById("score1");
-scoreB1.value = player1Score;
-
-var scoreB2 = document.getElementById("score2");
-scoreB2.value = player2Score;
 
 var aBtn = document.getElementById("a");
 var bBtn = document.getElementById("b");
@@ -53,143 +48,107 @@ var eBtn = document.getElementById("e");
 var fBtn = document.getElementById("f");
 
 var mainL = document.getElementById("mainLine");
+mainL.innerHTML = triviaQ[0];
 
-var player1 = function(){
+/*Gotta get a winner*/
 
-}
-
-var getScore = function(player1, player2){
-/*console.log(count);*/
-  if(count == 0){
-    console.log(aBtn);
+var winner = function(){
+  if(player1Score == player2Score){
+    return "Tie game!";
+  }else if(player1Score > player2Score){
+    return "Player 1 wins!";
+  }else{
+    return "Player 2 wins!";
   }
 }
 
-mainL.innerHTML = triviaQ[0];
-/*console.log(gameOn);*/
+/*reset color*/
+
 var continueBtn = document.getElementById("continue");
 
+var resetColor = function(){
+  mainL.style.color = "black";
+}
+
 /*Make continueBtn diplay question 1*/
+
 var count = 0;
 continueBtn.addEventListener("click", function(){
-  if (gameOn==false){
-    gameOn = true;
-  }
+
+  compSheet1();
+  compSheet2();
+
   count++;
-  console.log(player1());
   mainL.innerHTML = triviaQ[count];
-  if (count >= 9){
-    var c = confirm("Wanna play again?");
-    if (c == true){
+  scoreB1.value = player1Score;
+  scoreB2.value = player2Score;
+  if (count > 9){
+    var c = confirm(winner() + " Wanna play again?");
+    if (c === true){
     count = -1;
-    } else if (c == false){
+    player1Score = -1;
+    player2Score = -1;
+    answerSheet1 = [];
+    answerSheet2 = [];
+    mainL.innerHTML = "Press continue to play";
+    mainL.style.color = "red";
+    return;
+    } else if (c === false){
+      player1Score = -1;
+      player2Score = -1;
+      mainL.innerHTML = "GoodBye :)";
       txt = "Goodbye :)";
-      gameOn = false;
-      return gameOn;
     }
   }
-  return count;
+  return count, resetColor();
 });
 
+/*Tie DOM in buttons*/
+
 aBtn.addEventListener("click", function(){
-  console.log(aBtn);
   answerSheet1.push(this.innerHTML);
   console.log(answerSheet1);
 });
 bBtn.addEventListener("click", function(){
-  console.log(bBtn);
   answerSheet1.push(this.innerHTML);
   console.log(answerSheet1);
 });
 cBtn.addEventListener("click", function(){
-  console.log(cBtn);
   answerSheet1.push(this.innerHTML);
   console.log(answerSheet1);
 });
 dBtn.addEventListener("click", function(){
-  console.log(dBtn);
   answerSheet2.push(this.innerHTML);
   console.log(answerSheet2);
 });
 eBtn.addEventListener("click", function(){
-  console.log(eBtn);
   answerSheet2.push(this.innerHTML);
   console.log(answerSheet2);
 });
 fBtn.addEventListener("click", function(){
-  console.log(fBtn);
   answerSheet2.push(this.innerHTML);
   console.log(answerSheet2);
 });
 
+/*console.log(player1Score);*/
+
+var scoreB1 = document.getElementById("score1");
+scoreB1.value = player1Score;
+
+/*Compare answers*/
+
 var compSheet1 = function(){
-  if (count == 0){
-    if (answerSheet1[0] === answerKey[0]){
-      player1Score = player1Score + 1;
-      console.log(player1Score);
-    } else {}
+  if(answerSheet1.pop(count)===answerKey[count]){
+    player1Score++;
   }
-  if (count == 1) {
-    if (answerSheet1[1] === answerKey[1]){
-      player1Score = player1Score + 1;
-      console.log(player1Score);
-    } else {}
-  }
-  if (count == 2) {
-    if (answerSheet1[2] === answerKey[2]){
-      player1Score = player1Score + 1;
-      console.log(player1Score);
-    } else {}
-  }
-  if (count == 3) {
-    if (answerSheet1[3] === answerKey[3]){
-      player1Score = player1Score + 1;
-      console.log(player1Score);
-    } else {}
-  }
-  if (count == 4) {
-    if (answerSheet1[4] === answerKey[4]){
-      player1Score = player1Score + 1;
-      console.log(player1Score);
-    } else {}
-  }
-  if (count == 5) {
-    if (answerSheet1[5] === answerKey[5]){
-      player1Score = player1Score + 1;
-      console.log(player1Score);
-    } else {}
-  }
-  if (count == 6) {
-    if (answerSheet1[6] === answerKey[6]){
-      player1Score = player1Score + 1;
-      console.log(player1Score);
-    } else {}
-  }
-  if (count == 7) {
-    if (answerSheet1[7] === answerKey[7]){
-      player1Score = player1Score + 1;
-      console.log(player1Score);
-    } else {}
-  }
-  if (count == 8) {
-    if (answerSheet1[8] === answerKey[8]){
-      player1Score = player1Score + 1;
-      console.log(player1Score);
-    } else {}
-  }
-  if (count == 9) {
-    if (answerSheet1[9] === answerKey[9]){
-      player1Score = player1Score + 1;
-      console.log(player1Score);
-    } else {}
-  }
-  console.log(player1Score);
 };
 
+/*console.log(player2Score);*/
+var scoreB2 = document.getElementById("score2");
+scoreB2.value = player2Score;
 
-
-
-
-
-
-
+var compSheet2 = function(){
+  if(answerSheet2.pop(count)===answerKey[count]){
+    player2Score++;
+  }
+};
